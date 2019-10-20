49024473cdcce3ae1ef248d4d57c70e37def4e51
dtokazaki <25940882+dtokazaki@users.noreply.github.com>
2019-10-20 15:35:01 -0700

    starting threading

---
+++ convert.py
@@ -0,0 +1,10 @@
+convertion {
+    'mine':mine,
+    'receive_block':recieve_block,
+    'new_transaction': new_transaction,
+    'receive_transaction': receieve_transaction,
+    'full_chain': full_chain,
+    'register_nodes': register_nodes,
+    'consensus': consensus,
+    'get_block': get_block
+}
