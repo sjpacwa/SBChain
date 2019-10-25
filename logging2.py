38e0fe23547d55c9bf58277d4dbe69581f5b9fc2
Stephen Pacwa <sjpacwa@gmail.com>
2019-10-24 18:43:15 -0700

    Removed 'logging2.py'
    - Spoke with Daniel, turns out this file wasn't being used anywhere
    anyway. Removed in favor of standard library logger.

--- logging2.py
+++
@@ -1,6 +0,0 @@
-from main import ip, port
-
-# In progress printing function
-# eventually will move to logging
-def auto_logger(message):
-    print("Node #: {} IP: {} Port: {}               Message: {}".format(0,ip,port,message))
