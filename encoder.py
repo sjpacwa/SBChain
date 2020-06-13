f0efe55a41de903896b4cb29ce60c906af3acc88
dtokazaki <25940882+dtokazaki@users.noreply.github.com>
2020-06-12 19:52:44 -0700

    removing complex decoder

--- encoder.py
+++ encoder.py
@@ -7,11 +7,3 @@ class ComplexEncoder(json.JSONEncoder):
             return obj.to_json()
         else:
             return json.JSONEncoder.default(self, obj)
-
-
-class ComplexDecoder(json.JSONDecoder):
-    def default(self, obj):
-        if hasattr(obj, 'to_string'):
-            return obj.to_string()
-        else:
-            return json.JSONDecoder.default(self, obj)
