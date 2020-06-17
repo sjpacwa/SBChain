ac8c870d809a4a8b6f8aaf3f41a51376eb750d79
dtokazaki <25940882+dtokazaki@users.noreply.github.com>
2020-06-16 19:30:48 -0700

    removing adding transactions, already handled

--- mine.py
+++ mine.py
@@ -293,10 +293,6 @@ def resolve_conflicts(block, history_copy, host_port, metadata, reward_transacti
         success = verify_block(history_copy, block_obj, blockchain_copy)
         blockchain_copy.add_block(block_obj)
 
-        # Add all transactions from the block to our history copy
-        for transaction in block_obj.transactions:
-            history_copy.add_transaction(transaction)
-
         if not success:
             logging.debug("Could not replace chain")
             return False
